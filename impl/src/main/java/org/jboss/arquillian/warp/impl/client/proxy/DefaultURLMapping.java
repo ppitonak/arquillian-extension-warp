a802056ce4c63a309557fa3dbebdf353dec2b4c8
Jiri Stefek <jstefek@redhat.com>
2014-10-06 12:09:18 +0200

    changed implementation of DefaultURLMapping#getProxyAddress() to be compilable with JDK 1.6, added maven compiler properties

--- impl/src/main/java/org/jboss/arquillian/warp/impl/client/proxy/DefaultURLMapping.java
+++ impl/src/main/java/org/jboss/arquillian/warp/impl/client/proxy/DefaultURLMapping.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -70,6 +71,10 @@ public class DefaultURLMapping implements URLMapping {
     }
 
     private InetAddress getProxyAddress() {
-        return Inet4Address.getLoopbackAddress();
+        try {
+            return Inet4Address.getByName(null);
+        } catch (UnknownHostException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
